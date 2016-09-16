e121f187bbfcc6fc63a582aa09172d1e104e38db
asansoe <asansoe@mect.it>
2016-09-16 14:50:15 +0200

    Fix Setup and update generation (2.0.10rc3): - Moved source directory to C:\mect_plugins - Saved general compilation Log - Used new clean virtual machine for building (3410)

--- setup/prepare_installer.bat
+++ setup/prepare_installer.bat
@@ -3,7 +3,8 @@
 SET REVISION="2.0.10rc3"
 SET SETUP_DIR=%~dp0
 SET OUT_DIR=%SETUP_DIR%
-SET IN_DIR="C:\Users\UserName\Documents\GitHub\mect_plugins"
+SET IN_DIR="C:\mect_plugins"
+SET PATH=%PATH%;C:\Qt485\desktop\mingw32\bin\
 
 SET TARGET_LIST=TP1043_01_A TP1043_01_B TP1043_01_C TP1057_01_A TP1057_01_B TP1070_01_A TP1070_01_B TP1070_01_C TP1070_01_D TPAC1006 TPAC1007_03 TPAC1007_04_AA TPAC1007_04_AB TPAC1007_04_AC TPAC1008_01 TPAC1008_02_AA TPAC1008_02_AB TPAC1008_02_AC TPAC1008_02_AD TPAC1008_02_AE TPAC1008_02_AF
 
@@ -31,9 +32,11 @@ echo.
 
 echo.
 echo ----------------------------------------
-echo Creating the installer verion %REVISION%
+echo Creating the installer version %REVISION%
 echo ----------------------------------------
 echo. 
+
+echo Creating the installer version %REVISION% > %OUT_DIR%\error.log
 
 SET ORIGINAL=%CD%
 
@@ -57,7 +60,7 @@ IF %DLLBUILD% == 1 (
 	cd /D %IN_DIR%\qt_plugins
 	time /t
 	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" distclean>nul 2>&1
-	"C:\Qt485\desktop\bin\qmake.exe" qt_designer_plugins.pro -r -spec win32-g++ "CONFIG+=release" -config store -config trend -config recipe -config alarms "DEFINES += ATCM_VERSION=\"%REVISION%\"" > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\desktop\bin\qmake.exe" qt_designer_plugins.pro -r -spec win32-g++ "CONFIG+=release" -config store -config trend -config recipe -config alarms "DEFINES += ATCM_VERSION=\"%REVISION%\"" >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during Building dll libraries
 		pause
@@ -65,7 +68,7 @@ IF %DLLBUILD% == 1 (
 		exit
 	)
 
-	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during Building dll libraries
 		pause
@@ -75,7 +78,7 @@ IF %DLLBUILD% == 1 (
 
 	rem remove the old files
  	del /q C:\Qt485\desktop\plugins\designer\atcm*
-	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" install > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" install >> %OUT_DIR%\error.log 2>&1
  	IF ERRORLEVEL 1 (
 		echo problem during installation dll libraries
 		pause
@@ -89,7 +92,7 @@ IF %DLLBUILD% == 1 (
 	cd /D %IN_DIR%\setup\SystemConf
 	time /t
 	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" distclean>nul 2>&1
-	"C:\Qt485\desktop\bin\qmake.exe" SystemConf.pro -r -spec win32-g++ "CONFIG+=release" > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\desktop\bin\qmake.exe" SystemConf.pro -r -spec win32-g++ "CONFIG+=release" >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during Building SystemConf
 		pause
@@ -97,14 +100,14 @@ IF %DLLBUILD% == 1 (
 		exit
 	)
 
-	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\desktop\mingw32\bin\mingw32-make.exe" >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during Building SystemConf
 		pause
 		cd %ORIGINAL%
 		exit
 	)
-	xcopy %IN_DIR%\setup\SystemConf\release\SystemConf.exe C:\Qt485\desktop\bin\SystemConf.exe /Q /Y > %OUT_DIR%\error.log 2>&1
+	xcopy %IN_DIR%\setup\SystemConf\release\SystemConf.exe C:\Qt485\desktop\bin\SystemConf.exe /Q /Y >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during template.
 		pause
@@ -119,7 +122,7 @@ IF %TEMPLATE% == 1 (
 	for /d %%a in ("C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-*") do rd /s /q "%%~a"
 	mkdir %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards
 	
-	xcopy %IN_DIR%\qt_templates\ATCM-template-project C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project /Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+	xcopy %IN_DIR%\qt_templates\ATCM-template-project C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project /Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during template.
 		pause
@@ -127,7 +130,7 @@ IF %TEMPLATE% == 1 (
 		exit
 	)
 
-	xcopy %IN_DIR%\qt_templates\ATCM-template-form-class C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-form-class /Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+	xcopy %IN_DIR%\qt_templates\ATCM-template-form-class C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-form-class /Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during template.
 		pause
@@ -137,7 +140,7 @@ IF %TEMPLATE% == 1 (
 
 	for /d %%a in (%TARGET_LIST%) do (
 		echo 	Installing %%~a...
-		xcopy "%IN_DIR%\qt_templates\ATCM-template-project-%%~a" "C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project-%%~a" /Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+		xcopy "%IN_DIR%\qt_templates\ATCM-template-project-%%~a" "C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project-%%~a" /Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 		IF ERRORLEVEL 1 (
 			echo problem during template.
 			pause
@@ -154,7 +157,7 @@ IF %TARGETBUILD% == 1 (
 	cd /D "%IN_DIR%"
 	time /t
 	"C:\Qt485\imx28\mingw\bin\mingw32-make.exe" distclean>nul 2>&1
-	"C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\bin\qmake.exe" qt_atcm.pro -r -spec linux-arm-gnueabi-g++ -config release -config store -config trend -config recipe -config alarms > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\bin\qmake.exe" qt_atcm.pro -r -spec linux-arm-gnueabi-g++ -config release -config store -config trend -config recipe -config alarms >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during crating make file for target libraries
 		pause
@@ -162,7 +165,7 @@ IF %TARGETBUILD% == 1 (
 		exit
 	)
 	
-	"C:\Qt485\imx28\mingw\bin\mingw32-make.exe" > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\imx28\mingw\bin\mingw32-make.exe" >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during building target libraries
 		pause
@@ -170,7 +173,7 @@ IF %TARGETBUILD% == 1 (
 		exit
 	)
 
-	"C:\Qt485\imx28\mingw\bin\mingw32-make.exe" install > %OUT_DIR%\error.log 2>&1
+	"C:\Qt485\imx28\mingw\bin\mingw32-make.exe" install >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during install target libraries
 		pause
@@ -188,10 +191,10 @@ rem ##############################################
 IF %QTPROJECT% == 1 (
 	echo Preparing Configuration into QtProject...
 	time /t
-	rem "c:\Program Files\7-Zip\7z.exe" u -r -mx1 "%OUT_DIR%\QtProject.7z" "%APPDATA%\QtProject\qtcreator\devices.xml" "%APPDATA%\QtProject\qtcreator\profiles.xml" "%APPDATA%\QtProject\qtcreator\qtversion.xml" "%APPDATA%\QtProject\qtcreator\toolchains.xml" "%APPDATA%\QtProject\qtcreator\externaltools\lupdate.xml" > %OUT_DIR%\error.log
+	rem "c:\Program Files\7-Zip\7z.exe" u -r -mx1 "%OUT_DIR%\QtProject.7z" "%APPDATA%\QtProject\qtcreator\devices.xml" "%APPDATA%\QtProject\qtcreator\profiles.xml" "%APPDATA%\QtProject\qtcreator\qtversion.xml" "%APPDATA%\QtProject\qtcreator\toolchains.xml" "%APPDATA%\QtProject\qtcreator\externaltools\lupdate.xml" >> %OUT_DIR%\error.log
 	del /q "%OUT_DIR%\QtProject.7z"
-	rem "c:\Program Files\7-Zip\7z.exe" u -r -mx1 "%OUT_DIR%\QtProject.7z" "%APPDATA%\QtProject" -x!QtCreator.db -x!QtCreator.ini -xr!"qtcreator\generic-highlighter" -xr!"qtcreator\json" -xr!qtcreator\macros -x!qtcreator\default.qws -x!qtcreator\helpcollection.qhc > %OUT_DIR%\error.log
-	"c:\Program Files\7-Zip\7z.exe" u -r -mx1 "%OUT_DIR%\QtProject.7z" "%OUT_DIR%\QtProject" > %OUT_DIR%\error.log
+	rem "c:\Program Files\7-Zip\7z.exe" u -r -mx1 "%OUT_DIR%\QtProject.7z" "%APPDATA%\QtProject" -x!QtCreator.db -x!QtCreator.ini -xr!"qtcreator\generic-highlighter" -xr!"qtcreator\json" -xr!qtcreator\macros -x!qtcreator\default.qws -x!qtcreator\helpcollection.qhc >> %OUT_DIR%\error.log
+	"c:\Program Files\7-Zip\7z.exe" u -r -mx1 "%OUT_DIR%\QtProject.7z" "%OUT_DIR%\QtProject" >> %OUT_DIR%\error.log
 	IF ERRORLEVEL 1 (
 	 	echo problem during creation 7z file
 	 	pause
@@ -212,7 +215,7 @@ IF %PREPARE_UPDATE% == 1 (
 	time /t
 	echo Copying dll...
 	mkdir %OUT_DIR%\Qt485\desktop\plugins\designer
-	xcopy C:\Qt485\desktop\plugins\designer\*.dll %OUT_DIR%\Qt485\desktop\plugins\designer /Q /Y > %OUT_DIR%\error.log 2>&1
+	xcopy C:\Qt485\desktop\plugins\designer\*.dll %OUT_DIR%\Qt485\desktop\plugins\designer /Q /Y >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during .dll copy.
 		pause
@@ -222,7 +225,7 @@ IF %PREPARE_UPDATE% == 1 (
 
 	echo Copying template...
 	mkdir %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards
-	xcopy C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project				/Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+	xcopy C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project				/Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during template.
 		pause
@@ -230,7 +233,7 @@ IF %PREPARE_UPDATE% == 1 (
 		exit
 	)
 
-	xcopy C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-form-class %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-form-class	/Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+	xcopy C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-form-class %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-form-class	/Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during template.
 		pause
@@ -240,7 +243,7 @@ IF %PREPARE_UPDATE% == 1 (
 
 	for /d %%a in (%TARGET_LIST%) do (
 		echo Installing %%~a...
-		xcopy C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project-%%~a %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project-%%~a				/Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+		xcopy C:\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project-%%~a %OUT_DIR%\Qt485\desktop\share\qtcreator\templates\wizards\ATCM-template-project-%%~a				/Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 		IF ERRORLEVEL 1 (
 			echo problem during template.
 			pause
@@ -251,7 +254,7 @@ IF %PREPARE_UPDATE% == 1 (
 	
 	echo Copying SystemConf...
 	mkdir %OUT_DIR%\Qt485\desktop\bin
-	xcopy C:\Qt485\desktop\bin\SystemConf.exe %OUT_DIR%\Qt485\desktop\bin /Q /Y > %OUT_DIR%\error.log 2>&1
+	xcopy C:\Qt485\desktop\bin\SystemConf.exe %OUT_DIR%\Qt485\desktop\bin /Q /Y >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during SystemConf copy.
 		pause
@@ -261,7 +264,7 @@ IF %PREPARE_UPDATE% == 1 (
 
 	echo Copying rootfs...
 	mkdir %OUT_DIR%\Qt485\imx28
-	xcopy C:\Qt485\imx28\rootfs %OUT_DIR%\Qt485\imx28\rootfs	/Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+	xcopy C:\Qt485\imx28\rootfs %OUT_DIR%\Qt485\imx28\rootfs	/Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during template.
 		pause
@@ -272,8 +275,8 @@ IF %PREPARE_UPDATE% == 1 (
 	echo Copying conf files...
 	mkdir %OUT_DIR%\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++
 	mkdir %OUT_DIR%\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\common
-	xcopy C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf %OUT_DIR%\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++	/Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
-	xcopy C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\common\mect.conf %OUT_DIR%\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\common	/Q /Y /E /S /I > %OUT_DIR%\error.log 2>&1
+	xcopy C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf %OUT_DIR%\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++	/Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
+	xcopy C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\common\mect.conf %OUT_DIR%\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\common	/Q /Y /E /S /I >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during template.
 		pause
@@ -294,7 +297,7 @@ IF %PREPARE_UPDATE% == 1 (
 	echo Creating archive...
 	IF EXIST Qt485_upd_rev%REVISION%.7z del /q Qt485_upd_rev%REVISION%.7z
 
-	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 %OUT_DIR%\Qt485_upd_rev%REVISION%.7z %OUT_DIR%\Qt485\imx28 > %OUT_DIR%\error.log 2>&1
+	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 %OUT_DIR%\Qt485_upd_rev%REVISION%.7z %OUT_DIR%\Qt485\imx28 >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during creation of 7z update archive.
 		pause
@@ -302,7 +305,7 @@ IF %PREPARE_UPDATE% == 1 (
 		exit
 	)
 
-	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 %OUT_DIR%\Qt485_upd_rev%REVISION%.7z %OUT_DIR%\Qt485\desktop > %OUT_DIR%\error.log 2>&1
+	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 %OUT_DIR%\Qt485_upd_rev%REVISION%.7z %OUT_DIR%\Qt485\desktop >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during creation of 7z update archive.
 		pause
@@ -314,7 +317,8 @@ IF %PREPARE_UPDATE% == 1 (
 	time /t
 
 	echo   Configurator files...
-	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\MectConfigurator.7z" "%OUT_DIR%\MectConfigurator" > %OUT_DIR%\error.log
+	IF EXIST %OUT_DIR%\MectConfigurator.7z del /q %OUT_DIR%\MectConfigurator.7z
+	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\MectConfigurator.7z" "%OUT_DIR%\MectConfigurator" >> %OUT_DIR%\error.log
 	IF ERRORLEVEL 1 (
 	 	echo problem during creation 7z file
 	 	pause
@@ -334,7 +338,7 @@ IF %INSTALL% == 1 (
 	echo Preparing files...
 	time /t
 	echo   Target files...
-	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\Qt485.7z" "C:\Qt485\imx28" -xr!rootfs -x!qt-everywhere-opensource-src-4.8.5\mkspecs\common\mect.conf -x!qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf  > %OUT_DIR%\error.log
+	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\Qt485.7z" "C:\Qt485\imx28" -xr!rootfs -x!qt-everywhere-opensource-src-4.8.5\mkspecs\common\mect.conf -x!qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf  >> %OUT_DIR%\error.log
 	IF ERRORLEVEL 1 (
 	 	echo problem during creation 7z file
 	 	pause
@@ -344,7 +348,7 @@ IF %INSTALL% == 1 (
 	echo   PC files...
 	copy "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf" "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf.mect"
 	copy "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf.ori" "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf"
-	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\Qt485.7z" "C:\Qt485\desktop"  -xr!atcm*.dll -xr!ATCM-template-* > %OUT_DIR%\error.log
+	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\Qt485.7z" "C:\Qt485\desktop"  -xr!atcm*.dll -xr!ATCM-template-* >> %OUT_DIR%\error.log
 	IF ERRORLEVEL 1 (
 		copy "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf.mect" "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf"
 		echo problem during creation 7z file
@@ -355,7 +359,7 @@ IF %INSTALL% == 1 (
 	copy "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf.mect" "C:\Qt485\imx28\qt-everywhere-opensource-src-4.8.5\mkspecs\linux-arm-gnueabi-g++\qmake.conf"
 
 	echo   Fonts files...
-	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\Fonts.7z" "%OUT_DIR%\Fonts" > %OUT_DIR%\error.log
+	"c:\Program Files\7-Zip\7z.exe" u -r -mx9 "%OUT_DIR%\Fonts.7z" "%OUT_DIR%\Fonts" >> %OUT_DIR%\error.log
 	IF ERRORLEVEL 1 (
 	 	echo problem during creation 7z file
 	 	pause
@@ -367,7 +371,7 @@ IF %INSTALL% == 1 (
 	echo Preparing setup...
 	time /t
 	cd /D "%SETUP_DIR%"
-	"c:\Program Files\NSIS\makensis.exe" /DREVISION=%REVISION% /DMECT_CONFIGURATOR_REVISION=%MECT_CONFIGURATOR_REVISION% SETUP_MECT_QT.nsi > %OUT_DIR%\error.log 2>&1
+	"c:\Program Files\NSIS\makensis.exe" /DREVISION=%REVISION% /DMECT_CONFIGURATOR_REVISION=%MECT_CONFIGURATOR_REVISION% SETUP_MECT_QT.nsi >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during creation of setup
 		pause
@@ -381,7 +385,7 @@ IF %UPDATE% == 1 (
 	echo Preparing update...
 	time /t
 	cd /D "%SETUP_DIR%"
-	"c:\Program Files\NSIS\makensis.exe" /DREVISION=%REVISION% /DMECT_CONFIGURATOR_REVISION=%MECT_CONFIGURATOR_REVISION% UPDATE_MECT_QT.nsi > %OUT_DIR%\error.log 2>&1
+	"c:\Program Files\NSIS\makensis.exe" /DREVISION=%REVISION% /DMECT_CONFIGURATOR_REVISION=%MECT_CONFIGURATOR_REVISION% UPDATE_MECT_QT.nsi >> %OUT_DIR%\error.log 2>&1
 	IF ERRORLEVEL 1 (
 		echo problem during creation of setup
 		pause
@@ -393,13 +397,11 @@ IF %UPDATE% == 1 (
 
 rem IF EXIST %OUT_DIR%\Qt485.7z del /q %OUT_DIR%\Qt485.7z
 
-IF EXIST %OUT_DIR%\MectConfigurator.7z del /q %OUT_DIR%\MectConfigurator.7z
+rem IF EXIST %OUT_DIR%\MectConfigurator.7z del /q %OUT_DIR%\MectConfigurator.7z
 
-IF EXIST %OUT_DIR%\error.log del /q %OUT_DIR%\error.log
+rem IF EXIST %OUT_DIR%\Qt485 RD /S /Q %OUT_DIR%\Qt485
 
-IF EXIST %OUT_DIR%\Qt485 RD /S /Q %OUT_DIR%\Qt485
-
-IF EXIST %OUT_DIR%\Qt485_upd_rev%REVISION%.7z del /q %OUT_DIR%\Qt485_upd_rev%REVISION%.7z
+rem IF EXIST %OUT_DIR%\Qt485_upd_rev%REVISION%.7z del /q %OUT_DIR%\Qt485_upd_rev%REVISION%.7z
 
 cd %ORIGINAL%
 
