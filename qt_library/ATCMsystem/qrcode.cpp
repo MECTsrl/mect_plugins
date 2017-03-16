dfea8b90d934936bdf7f9001cc8e8cf8894c7b2a
asansoe <asansoe@mect.it>
2017-03-16 12:51:30 +0100

    Updated QR Code geometry for Portrait or small screens

--- qt_library/ATCMsystem/qrcode.cpp
+++ qt_library/ATCMsystem/qrcode.cpp
@@ -3,6 +3,9 @@
 
 #include <QFileInfo>
 #include <QPixmap>
+#include <QRect>
+
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
 qrcode::qrcode(QString szFileQRC, QWidget *parent) :
     QDialog(parent),
@@ -13,9 +16,31 @@ qrcode::qrcode(QString szFileQRC, QWidget *parent) :
     this->setWindowState(Qt::WindowFullScreen);
     QFileInfo fInfo(szFileQRC);
     if (fInfo.exists())  {
+        QRect parentSize = parent->geometry();
+
+        // get Parent Screen Dimensions
+        int w = parentSize->width();
+        int h = parentSize->height();
+
+        // Find minimal size for Label
+        int minSize = MIN(w, h);
+        bool fLandScape = w > h;
+
+        if (! fLandScape)  {
+            minSize = minSize - ui->pushButtonBack->width();
+        }
+        // 2 Pixels space
+        minSize -= 4;
+        ui->lblQrCode->setMinimumSize(minSize, -minSize);
+        ui->lblQrCode->updateGeometry();
+        // Get final geometry of label
+        w = ui->lblQrCode->width();
+        h = ui->lblQrCode->height();
+        // Loading QR Code Pixmap
         QPixmap myPix;
         myPix.load(szFileQRC);
-        ui->lblQrCode->setPixmap(myPix);
+        // set a scaled pixmap to a w x h window keeping its aspect ratio
+        ui->lblQrCode->setPixmap(myPix.scaled(w,h,Qt::KeepAspectRatio));
     }
 }
 
