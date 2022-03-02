325c0324c34325ba69a1937962c453ce03842e61
Andrea SANSOE <asansoe@mect.it>
2022-03-02 15:31:25 +0100

    Bug #11 backlight_TPAC1008_2 e screensaver (based on PLC_PRODUCT_ID) fixed & tested

--- qt_library/ATCMcommon/screensaver.cpp
+++ qt_library/ATCMcommon/screensaver.cpp
@@ -42,12 +42,13 @@ void ScreenSaver::restore()
     {
         brightness_level = 100;
     }
-    // Get Model Number
+    // Get Model Number, at startup is 0
     readFromDbQuick(ID_PLC_PRODUCT_ID, (int *)&uModel);
-    uModel = uModel >> 2;
+    uModel = uModel >> 8;
+    // fprintf(stderr, "Model Family: 0x%08x - ON Screen\n", uModel);
     // Patch for 100802 Family
     if (uModel == 0x100802)  {
-        sprintf (command, "echo %d > %s", BLANK_SCREEN_FILE, 0);
+        sprintf (command, "echo %d > %s", 0, BLANK_SCREEN_FILE);
     }
     else  {
         sprintf (command, "echo %d > %s", brightness_level, BACKLIGHT_FILE_SYSTEM);
@@ -64,12 +65,13 @@ bool ScreenSaver::save(__attribute__((unused)) int level)
     char        command[256] = "";
     unsigned    uModel = 0;
 
-    // Get Model Number
+    // Get Model Number, at startup is 0
     readFromDbQuick(ID_PLC_PRODUCT_ID, (int *)&uModel);
-    uModel = uModel >> 2;
+    uModel = uModel >> 8;
+    // fprintf(stderr, "Model Family: 0x%08x - OFF Screen\n", uModel);
     // Patch for 100802 Family
     if (uModel == 0x100802)  {
-        sprintf (command, "echo %d > %s", BLANK_SCREEN_FILE, 1);
+        sprintf (command, "echo %d > %s", 1, BLANK_SCREEN_FILE);
     }
     else  {
         sprintf (command, "echo %d > %s", brightness_level, BACKLIGHT_FILE_SYSTEM);
