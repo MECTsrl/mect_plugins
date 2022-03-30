9fb2f947af031c4b59997224260fae3a78588fe1
Andrea Sansoe <asansoe@mect.it>
2022-03-30 18:17:44 +0200

    Fix TP1070_02_F new project wizard (missing automation.cpp file)

---
+++ qt_templates/ATCM-template-project-TP1070_02_F/automation.cpp
@@ -0,0 +1,24 @@
+#include "crosstable.h"
+#include "automation.h"
+
+/* put here the initalization */
+void setup(void)
+{
+    // Wait PLC Engine gets ready
+    while (PLC_EngineStatus < 2) {
+        fputc('*', stderr);
+        sleep(1);
+    }
+    // FastIO Configuration
+    doWrite_PLC_FastIO_Ena(0x000000FF);
+    doWrite_PLC_FastIO_Dir(0x000000F0);
+    // Insert your start-up code here
+    // .....
+}
+
+/* put here the operation made every 100ms */
+void loop(void)
+{
+    
+}
+
